9b22b9019afab6445746094c867de1c9ebcedcc3
Mahmoud Alnkeeb <59762294+mahmoudalnkeeb@users.noreply.github.com>
2021-11-13 01:07:16 +0200

    upload the project

---
+++ server.js
@@ -0,0 +1,48 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express to run server and routes
+const express = require("express");
+
+// Start up an instance of app
+
+const app = express();
+
+/* Middleware*/
+
+const bodyParser = require("body-parser");
+
+//Here we are configuring express to use body-parser as middle-ware.
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+
+const cors = require("cors");
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static("website"));
+// TODO-Spin up the server
+app.post("/add", callBack);
+
+function callBack(req, res) {
+    res.send("POST received");
+}
+const data = [];
+
+app.post("/animal", addAnimal);
+
+function addAnimal(req, res) {
+    data.push(req.body);
+}
+// Setup Server
+const port = 3000;
+
+const server = app.listen(port, listening);
+
+function listening() {
+    console.log(server);
+    console.log(`running on localhost: ${port}`);
+}
